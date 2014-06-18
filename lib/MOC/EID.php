830afecd7061b964fc0d40725cce2dbeaff301cb
www-data <www-data@mocsystems.com>
2014-06-18 14:34:56 +0200

    [!!!][TASK] Update code base to TYPO3 6.2 compatibility

--- lib/MOC/EID.php
+++ lib/MOC/EID.php
@@ -13,9 +13,6 @@ class MOC_EID {
 		self::connectDB();
 		self::createTSFE();
 
-		require_once(PATH_t3lib. 'class.t3lib_userauth.php');
-		require_once(PATH_tslib. 'class.tslib_feuserauth.php');
-
 		// Initialize FE user:
 		$GLOBALS['TSFE']->initFEuser();
 
@@ -29,16 +26,10 @@ class MOC_EID {
 
 	public static function createTSFE() {
 		global $TYPO3_CONF_VARS;
-		$temp_TSFEclassName = 'tslib_fe';
+		$temp_TSFEclassName = 'TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController';
 		if(!is_a($GLOBALS['TSFE'],$temp_TSFEclassName)) {
-			// Include classes necessary for initializing frontend user:
-			// We will use tslib_fe to do that:
-			require_once(PATH_tslib.'class.tslib_fe.php');
-			require_once(PATH_t3lib.'class.t3lib_cs.php');
-
 			// Make new instance of TSFE object
-			$GLOBALS['TSFE'] = new $temp_TSFEclassName($TYPO3_CONF_VARS,0,0,true);
-
+			$GLOBALS['TSFE'] = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance($temp_TSFEclassName, $TYPO3_CONF_VARS, 0, 0, TRUE);
 		}
 	}
 
@@ -50,39 +41,32 @@ class MOC_EID {
 	public static function cObjOnTSFE() {
 		self::initFeUser();
 		self::createTSFE();
-        
-		require_once(PATH_t3lib.'class.t3lib_page.php');
-        
-		$GLOBALS['TSFE']->sys_page = t3lib_div::makeInstance('t3lib_pageSelect');
+
+		$GLOBALS['TSFE']->sys_page = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('\TYPO3\CMS\Frontend\Page\PageRepository');
 		$GLOBALS['TSFE']->initTemplate();
 		$GLOBALS['TSFE']->determineId();
 		$GLOBALS['TSFE']->getConfigArray();
-		require_once(PATH_tslib.'class.tslib_content.php');
 		$GLOBALS['TSFE']->newCObj();
 	}
-    
-    /**
-     * Get an instance of tslib_pibase
-     * 
-     * @param boolean $withCObject Should an cObj be attached to the tslib_pibase object
-     * @return tslib_pibase
-     */ 
-    public static function getInstanceOfPiBase($withCObject = true) {
-        if (!class_exists('tslib_pibase', false)) {
-            require_once PATH_tslib . 'class.tslib_pibase.php';
-        }
-        
-        static $PiBase;
-        
-        if (empty($PiBase)) {
-            $PiBase = new tslib_pibase();
-        }
-                    
-        if ($withCObject && empty($PiBase->cObj)) {
-            MOC_EID::cObjOnTSFE();
-            $PiBase->cObj = $GLOBALS['TSFE']->cObj;
-        }
-        
-        return $PiBase;
-    }
+
+	/**
+	 * Get an instance of tslib_pibase
+	 * 
+	 * @param boolean $withCObject Should an cObj be attached to the \TYPO3\CMS\Frontend\Plugin\AbstractPlugin object
+	 * @return \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
+	 */ 
+	public static function getInstanceOfPiBase($withCObject = TRUE) {
+		static $PiBase;
+
+		if (empty($PiBase)) {
+			$PiBase = new \TYPO3\CMS\Frontend\Plugin\AbstractPlugin();
+		}
+
+		if ($withCObject && empty($PiBase->cObj)) {
+			MOC_EID::cObjOnTSFE();
+			$PiBase->cObj = $GLOBALS['TSFE']->cObj;
+		}
+
+		return $PiBase;
+	}
 }
